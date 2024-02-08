1ae03ed3bf8eabf62ff877efa0cc99702b9e799c
Christia Kusmanow <christian.kusmanow@teslasoft.de>
2024-02-08 20:26:24 +0100

    refactor(public/css/v4): create css bundle using sass & postcss
    The jquery-ui.css file was removed and replaced with a more efficient version created using SASS and PostCSS. It restructures the file organization for more efficient usage and better readability. The change aims to enhance the performance and maintainability of the CSS codebase.

---
+++ public/js/v4/postcss.config.js
@@ -0,0 +1,15 @@
+const plugins = [
+    require('tailwindcss'),
+    require('autoprefixer'),
+];
+
+if (process.env.NODE_ENV === 'production') {
+    plugins.push(require('cssnano')({
+        preset: ['default',
+            {
+                discardComments: { removeAll: true },
+            }],
+    }));
+}
+
+module.exports = { plugins };
